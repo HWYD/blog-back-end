b0948031834c1cd83190d5cea5ac3dd1d71a7ef7
weiyue.huang <weiyue.huang@adspower.net>
2025-01-22 18:26:51 +0800

    添加cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mountRouters from './routes/mountRouters.js'
 import mountMiddleware from './middleware/index.js'
-
+import cors from 'cors';
 
 
 
@@ -9,12 +9,21 @@ const PORT = 3300 // 用于设置端口号
 const app = express() // 创建一个express应用程序实例
 app.use(express.static('public'))
 
+// 或者只允许特定源的请求
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 // app.use(responseFormatter)
 
 mountMiddleware(app)
 
 // mountMethodDemo(app)
 mountRouters(app)
+
 
 app.listen(PORT, () => {
     // 在控制台输出服务器运行信息
