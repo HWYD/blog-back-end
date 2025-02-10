c4d550bffd37334c6e84b3cfcfd523d088b65734
weiyue.huang <weiyue.huang@adspower.net>
2025-02-10 13:36:25 +0800

    配置环境

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 import Sequelize from 'sequelize'
 
-const sequelize = new Sequelize('blog_db', 'root', 'mysql3306', {
+
+const db_password = process.env.NODE_ENV == 'development'? 'mysql3306' : '3324742.aA'
+
+
+const sequelize = new Sequelize('blog_db', 'root', db_password, {
   host: '127.0.0.1',
   dialect: 'mysql',
   port: 3306,
